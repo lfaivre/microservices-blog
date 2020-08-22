b1a955b561e575ca105ce9d2600f29f0dbbb2ca3
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-08-22 04:51:52 -0700

    feat/refactor: add prettier and format files for client application

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 export default ({ postId }) => {
-  const [content, setContent] = useState('');
+  const [content, setContent] = useState("");
 
   const onSubmit = async (event) => {
     event.preventDefault();
     await axios.post(`http://posts.com/posts/${postId}/comments`, {
       content,
     });
-    setContent('');
+    setContent("");
   };
 
   return (
--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export default ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     let content;
-    if (comment.status === 'approved') {
+    if (comment.status === "approved") {
       content = comment.content;
     }
-    if (comment.status === 'pending') {
-      content = 'This comment is awaiting moderation.';
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation.";
     }
-    if (comment.status === 'rejected') {
-      content = 'This comment has been rejected by the moderator.';
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected by the moderator.";
     }
     return <li key={comment.id}>{content}</li>;
   });
