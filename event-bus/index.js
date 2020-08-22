cfc14de2b6a186715f215305ccd5f508b84db0f3
Lorenzo Faivre <lorenzo.faivre@gmail.com>
2020-08-22 04:45:08 -0700

    feat/refactor: add prettier and format files

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,21 +8,21 @@ app.use(bodyParser.json());
 const events = [];
 
 app.post('/events', (req, res) => {
-  const event = req.body;
-  events.push(event);
+	const event = req.body;
+	events.push(event);
 
-  axios.post('http://posts-clusterip-srv:4000/events', event);
-  axios.post('http://comments-srv:4001/events', event);
-  axios.post('http://query-srv:4002/events', event);
-  axios.post('http://moderation-srv:4003/events', event);
+	axios.post('http://posts-clusterip-srv:4000/events', event);
+	axios.post('http://comments-srv:4001/events', event);
+	axios.post('http://query-srv:4002/events', event);
+	axios.post('http://moderation-srv:4003/events', event);
 
-  res.send({ status: 'OK' });
+	res.send({ status: 'OK' });
 });
 
 app.get('/events', (req, res) => {
-  res.send(events);
+	res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log('Listening on 4005...');
+	console.log('Listening on 4005...');
 });
--- moderation/index.js
+++ moderation/index.js
@@ -6,26 +6,26 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', async (req, res) => {
-  console.log('Received Event:', req.body.type);
-  const { type, data } = req.body;
+	console.log('Received Event:', req.body.type);
+	const { type, data } = req.body;
 
-  if (type === 'CommentCreated') {
-    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+	if (type === 'CommentCreated') {
+		const status = data.content.includes('orange') ? 'rejected' : 'approved';
 
-    await axios.post('http://event-bus-srv:4005/events', {
-      type: 'CommentModerated',
-      data: {
-        id: data.id,
-        postId: data.postId,
-        status,
-        content: data.content,
-      },
-    });
-  }
+		await axios.post('http://event-bus-srv:4005/events', {
+			type: 'CommentModerated',
+			data: {
+				id: data.id,
+				postId: data.postId,
+				status,
+				content: data.content,
+			},
+		});
+	}
 
-  res.send({});
+	res.send({});
 });
 
 app.listen(4003, () => {
-  console.log('Listening on port 4003...');
+	console.log('Listening on port 4003...');
 });
